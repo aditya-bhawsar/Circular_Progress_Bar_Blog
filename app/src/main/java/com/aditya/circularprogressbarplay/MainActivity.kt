541446e8a50e4c834b0baf65297c5c5f6b6cb7f4
aditya-bhawsar <aditya.bhawsar98@gmail.com>
2020-10-14 03:38:01 +0530

    New Progress Bar Added

--- app/src/main/java/com/aditya/circularprogressbarplay/MainActivity.kt
+++ app/src/main/java/com/aditya/circularprogressbarplay/MainActivity.kt
@@ -2,10 +2,25 @@ package com.aditya.circularprogressbarplay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun setProgressTo(progress: Int){
+        progress_tv.text = "$progress%"
+        circular_determinative_pb.progress = progress
+    }
+
+    fun setSecondaryProgressTo(progress: Int){
+        circular_determinative_pb.secondaryProgress = progress
+    }
+
+
+
 }
